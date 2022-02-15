e39df3aaae822eeb0842c27d3de788b4c82d3a3a
cycxtIT <90236905+cycxtIT@users.noreply.github.com>
2022-02-15 17:08:24 +0800

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -11,10 +11,10 @@ if HEROKU:
 if not HEROKU:
 
     bot_token = "5045995046:AAGZOP46w96nXh5ORg9vIL8bekHLe3_zjEI"
-    ARQ_API_KEY = "XNGIAT-CFTXQS-MEDCWV-SWVHYC-ARQ"
+    ARQ_API_KEY = "HTRZML-SWNANX-XKEZWB-ERDDRI-ARQ"
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
     LANGUAGE = "zh-cn"
 
 # Leave it as it is
-ARQ_API_BASE_URL = "https://thearq.tech"
+ARQ_API_BASE_URL = "https://thearq.tech?username=%E7%A8%8B%E6%BA%90"
